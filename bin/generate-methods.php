194f1fb1f1442e6096c742feef69e0c426e561fa
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-04-06 04:02:01 -0700

    Setup generator options

--- bin/generate-methods.php
+++ bin/generate-methods.php
@@ -10,10 +10,11 @@ $generator_options_file = __DIR__ . '/generator_options.yml';
 
 // Options definition: the configuration file parameter is optional
 $options = GeneratorOptions::instance($generator_options_file);
-$options
-    ->setOrigin('http://track.smsaexpress.com/SECOM/SMSAwebService.asmx?WSDL')
-    ->setDestination('./MySdk')
-    ->setComposerName('myproject/mysdk');
+
+$options->setAddComments([
+    "date" => date("Y/m/d"),
+    "codeVersion" => "0.0.1",
+]);
 
 // Generator instantiation
 $generator = new Generator($options);
